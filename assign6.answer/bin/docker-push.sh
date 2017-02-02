4f12dc80c78ed2582a2e9267b7dca99a4277fb8d
pdxdive <ddcarr@gmail.com>
2017-02-02 15:37:18 -0800

    assign 6 and 7

--- assign6.answer/bin/docker-push.sh
+++ assign6.answer/bin/docker-push.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-# Push only if it's not a pull request
+# Tag, Push and Deploy only if it's not a pull request
 if [ "$TRAVIS_BRANCH" == "master" ]; then
     docker login --username "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD"
-    docker push pdxdiver/django-web &&
+    docker push pdxdiver/django-web:latest
     ecs-cli compose --project-name ecs-hacko --file ecs-deploy.yml service up;
 fi
--- assign7/bin/config-ecs-cli.sh
+++ assign7/bin/config-ecs-cli.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Download, Install and Configure ecs-cli
 sudo curl -o /usr/local/bin/ecs-cli https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-linux-amd64-latest
 sudo chmod +x /usr/local/bin/ecs-cli
 
